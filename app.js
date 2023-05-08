c7b7aaeb622f3728c411f4f150973934e94a823e
Belal <mohammadvns121@gmail.com>
2023-05-08 17:16:37 +0530

    Intro to Middlewares

--- app.js
+++ app.js
@@ -1,17 +1,21 @@
 
-const express = require('express');
+// const express = require('express');
+// const app = express();
 
-const app = express();
-
-// app.use((req,res,next)=>{
-//     console.log('In the middleware')
-//     next(); // allows the request to continue to the next middleware in line
-// }) // use allowes us to use new middlewares functions 
-
-// app.use((req,res,next)=>{
+// app.use('/add-product', (req, res, next) => {
 //     console.log('In the another middleware')
+//     res.send('<form action="/product" method="POST"><input type="text" name="title"><button type="submit">Add product</button></form>')
 // })
 
+// app.use('/product', (req, res, next) => {
+//     console.log(req.body); //undefined because we have to parse our incomming body
+//     res.redirect('/')
+// });
+
+// app.use('/', (req, res, next) => {
+//     console.log('In the another middleware')
+//     res.send('<h1>Hello from Express!</h1>')
+// })
 // app.listen(8000);
 
 
@@ -23,22 +27,42 @@ const app = express();
 
 
 
-app.use((req,res,next)=>{
-    console.log('In the middleware')
-    next();
-}) 
 
-app.use((req,res,next)=>{
-    console.log('In the another middleware')
-    res.send('<h1>Hello from Express!</h1>')  ///// content-Type: text/html;
+
+
+
+
+
+
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const app = express();
+
+app.use(bodyParser.urlencoded({extended: true}))
+
+app.use('/add-product', (req, res, next) => {
+    res.send('<form action="/product" method="POST"><input type="text" name="title" placeholder="Add-Product"><input type="number" name="title" placeholder="size"><button type="submit">Add product</button></form>')
 })
 
-// app.use((req,res,next)=>{
+// app.use('/product', (req, res, next) => {  ///we can also use app.get() or app.post() instead of app.use
 
-//     res.send( { key1: "value"})     /////content-Type:	json
-// })
+//     console.log(req.body);
+//     res.redirect('/')
+// });
 
 
 
+app.post('/product', (req, res, next) => { 
+    console.log(req.body);
+    res.redirect('/')
+});
+
+
+app.use('/', (req, res, next) => {
+    res.send('<h1>Hello from Express!</h1>')
+})
 
 app.listen(8000);
+
+
